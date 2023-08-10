d5c52e4cf79182958bbd1e66a3e1948f0b9b2571
Avinash <avinash.90527@gmail.com>
2023-08-10 23:41:46 +0530

    cors updated

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5000",
   "http://localhost:5173",
+  "https://recipen.vercel.app",
+  "https://recipen-backend.vercel.app",
 ];
 
 module.exports = allowedOrigins;
